15eb18a485d1e11dc29d5a17399addf5c53570a6
nicolas <nicolassosamachado25@gmail.com>
2023-07-30 14:39:05 -0300

    Reiniciando o repositorio

--- README.md
+++
@@ -1,3 +0,0 @@
-# Projetos-Flutter-Cursos
-
-Recomeçando o repositorio
--- middlewares/error.js
+++
@@ -1,7 +0,0 @@
-const errorMiddleware = async (res, err) => {
-  const { status = 500, message = "Internal Server Error", data = {} } = err;
-
-  return res.status(status).send({ message });
-};
-
-module.exports = errorMiddleware;
