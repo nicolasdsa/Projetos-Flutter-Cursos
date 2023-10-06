4b2fb748f4c0f45ddd39455f5e4c9aea33eeaeda
nicolas <nicolassosamachado25@gmail.com>
2023-10-05 23:36:46 -0300

    começando outro projeto

---
+++ meals/lib/components/category_item.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+  final Category category;
+
+  const CategoryItem(this.category, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.5),
+            category.color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Text(category.title),
+    );
+  }
+}
