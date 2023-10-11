e02f58c1d09571f387ece1124bd6e3ab80db60d9
nicolas <nicolassosamachado25@gmail.com>
2023-10-11 00:02:41 -0300

    utilização dos providers

---
+++ shop/lib/components/product_item.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product.dart';
+import 'package:shop/utils/app_routes.dart';
+
+class ProductItem extends StatelessWidget {
+  const ProductItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final product = Provider.of<Product>(
+      context,
+      listen: false,
+    );
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: Consumer<Product>(
+            builder: (ctx, product, _) => IconButton(
+              onPressed: () {
+                product.toggleFavorite();
+              },
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.shopping_cart),
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+        ),
+        child: GestureDetector(
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+          ),
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              AppRoutes.productDetail,
+              arguments: product,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ shop/lib/pages/products_overview_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:shop/components/product_grid.dart';
+
+class ProductsOverviewPage extends StatelessWidget {
+  const ProductsOverviewPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Minha Loja'),
+      ),
+      body: const ProductGrid(),
+    );
+  }
+}
