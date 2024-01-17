3bcc6382d1d16b5f4f8c2a1af26edfb5a649b6f4
nicolas <nicolassosamachado25@gmail.com>
2024-01-17 02:17:07 -0300

    instalando sharedPreferences

--- journal_webapi_authentication/lib/services/auth_service.dart
+++ journal_webapi_authentication/lib/services/auth_service.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:http_interceptor/http/intercepted_client.dart';
 import 'package:journal_webapi_authentication/services/http_interceptors.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
   static const String url = "http://10.0.2.2:3000";
@@ -17,10 +18,10 @@ class AuthService {
         headers: {'Content-type': 'application/json'},
         body: json.encode({'email': email, 'password': password}));
 
-    Map<String, dynamic> responseData = json.decode(response.body);
-    String message = responseData['message'];
+    if (response.statusCode != 200) {
+      Map<String, dynamic> responseData = json.decode(response.body);
+      String message = responseData['message'];
 
-    if (response.statusCode != 202) {
       switch (message) {
         case 'User not found':
           throw UserNotFindException();
@@ -29,6 +30,7 @@ class AuthService {
       }
     }
 
+    saveUserInfos(response.body);
     return true;
   }
 
@@ -36,6 +38,25 @@ class AuthService {
     http.Response response = await client.post(Uri.parse('$url/auth/signup'),
         headers: {'Content-type': 'application/json'},
         body: json.encode({'email': email, 'password': password}));
+
+    if (response.statusCode != 202) {
+      throw HttpException(response.body);
+    }
+
+    saveUserInfos(response.body);
+  }
+
+  saveUserInfos(String body) async {
+    Map<String, dynamic> responseData = json.decode(body);
+
+    String token = responseData['access_token'];
+    String email = responseData['email'];
+    String id = responseData['id'];
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString("accessToken", token);
+    prefs.setString("email", email);
+    prefs.setString("id", id);
   }
 }
 
--- journal_webapi_authentication/lib/services/http_interceptors.dart
+++ journal_webapi_authentication/lib/services/http_interceptors.dart
@@ -2,33 +2,23 @@ import 'package:http_interceptor/http_interceptor.dart';
 import 'package:logger/logger.dart';
 
 class LoggingInterceptor implements InterceptorContract {
-  Logger logger = Logger();
+  Logger logger = Logger(printer: PrettyPrinter(methodCount: 0));
+
   @override
-  Future<BaseRequest> interceptRequest({required BaseRequest request}) async {
-    logger.t("Requisição para ${request.url}\nCabeçalho:${request.headers}");
-    return request;
+  Future<RequestData> interceptRequest({required RequestData data}) async {
+    logger.v("Requisição para: ${data.baseUrl}\n${data.headers}");
+    return data;
   }
 
   @override
-  Future<BaseResponse> interceptResponse(
-      {required BaseResponse response}) async {
-    if (response.statusCode ~/ 100 == 2) {
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+    if (data.statusCode ~/ 100 == 2) {
       logger.i(
-          "Resposta de ${response.request?.url}\n${response.headers}\n${response.statusCode}\n");
+          "Resposta de ${data.url}\n${data.headers}\n${data.statusCode}\n ${data.body}");
     } else {
       logger.e(
-          "Resposta de ${response.request?.url}\n${response.headers}\n${response.statusCode}\n");
+          "Resposta de ${data.url}\n${data.headers}\n${data.statusCode}\n ${data.body}");
     }
-    return response;
-  }
-
-  @override
-  Future<bool> shouldInterceptRequest() async {
-    return true;
-  }
-
-  @override
-  Future<bool> shouldInterceptResponse() async {
-    return true;
+    return data;
   }
 }
