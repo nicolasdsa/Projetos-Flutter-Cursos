6547a69a49552c05700e8ef207105013901c1b6a
nicolas <nicolassosamachado25@gmail.com>
2024-02-15 17:56:17 -0300

    Iniciando curso

---
+++ client_control_tests/android/app/src/main/kotlin/com/example/client_control_tests/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.client_control_tests
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ client_control_tests/integration_test/app_test.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:client_control/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('Integration Test', (tester) async {
+    app.main();
+    await tester.pumpAndSettle();
+    expect(find.text('Clientes'), findsOneWidget);
+    expect(find.byIcon(Icons.menu), findsOneWidget);
+    expect(find.byType(FloatingActionButton), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.menu));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Menu'), findsOneWidget);
+    expect(find.text('Gerenciar clientes'), findsOneWidget);
+    expect(find.text('Tipos de clientes'), findsOneWidget);
+    expect(find.text('Sair'), findsOneWidget);
+
+    await tester.tap(find.text('Tipos de clientes'));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Tipos de cliente'), findsOneWidget);
+    expect(find.byType(FloatingActionButton), findsOneWidget);
+    expect(find.byIcon(Icons.menu), findsOneWidget);
+    expect(find.text('Platinum'), findsOneWidget);
+    expect(find.text('Golden'), findsOneWidget);
+    expect(find.text('Titanium'), findsOneWidget);
+    expect(find.text('Diamond'), findsOneWidget);
+
+    await tester.tap(find.byType(FloatingActionButton));
+    await tester.pumpAndSettle();
+    expect(find.byType(AlertDialog), findsOneWidget);
+    await tester.enterText(find.byType(TextFormField), 'Ferro');
+
+    await tester.tap(find.text('Selecionar icone'));
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.byIcon(Icons.card_giftcard));
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.text('Salvar'));
+    await tester.pumpAndSettle();
+    expect(find.text('Ferro'), findsOneWidget);
+    expect(find.byIcon(Icons.card_giftcard), findsOneWidget);
+  });
+}
